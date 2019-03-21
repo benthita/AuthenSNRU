805b05cf0e753aa7cef8148cbe046bd1c4458eae
Pring123 <benbentitayenbamrung@gmail.com>
2019-03-21 10:42:21 +0700

    Get Password Success

--- AuthenSNRU/RegisterViewController.swift
+++ AuthenSNRU/RegisterViewController.swift
@@ -35,10 +35,14 @@ class RegisterViewController: UIViewController {
         print("You Click upload")
         
         let name = nameTextField.text!
+        let user = userTextField.text!
+        let password = passwordTextField.text!
         
         
         
         print("name = \(name)")
+        print("user  = \(user)")
+        print("password = \(password)")
     }
     
     @IBAction func backButton(_ sender: UIBarButtonItem) {
